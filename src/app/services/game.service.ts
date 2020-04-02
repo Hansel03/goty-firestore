4a28339707efa267e5f6ebbceec437de8c4ca3f2
Hansel <hanselcapillo@hotmail.com>
2020-04-01 21:52:03 -0500

    Mostrar los juegos en el HTML

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,14 +2,22 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../environments/environment";
 import { Game } from "../interfaces/interfaces";
+import { of } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class GameService {
+  private juegos: Game[] = [];
   constructor(private http: HttpClient) {}
 
   public getNominados() {
-    return this.http.get<Game[]>(`${environment.url}/api/goty`);
+    if (this.juegos.length > 0) {
+      return of(this.juegos);
+    }
+    return this.http
+      .get<Game[]>(`${environment.url}/api/goty`)
+      .pipe(tap(response => (this.juegos = response)));
   }
 }
